37dce2f394f390f72274d332fa517055ee481f4a
Pritam Bandgar <157994717+Preetbandgar@users.noreply.github.com>
2026-07-12 03:30:01 +0530

    Refactor Pose subscriber

--- src/my_robot_package/my_robot_package/pose_subscriber.py
+++ src/my_robot_package/my_robot_package/pose_subscriber.py
@@ -6,12 +6,15 @@ from geometry_msgs.msg import PoseStamped
 class PoseTrackingSubscriber(Node):
     def __init__(self):
         super().__init__('pose_tracking_node')
-        self.subscription_ = self.create_subscription(PoseStamped, 'robot_pose', self.timer_callback, 10)
+        self.subscription_ = self.create_subscription(PoseStamped, 'robot_pose', self.pose_callback, 10)
         self.get_logger().info('The pose tracking node has been started')
 
-    def timer_callback(self, msg):
-        msg = PoseStamped()
-        self.get_logger().info(f"[INFO] [PoseTrackingSubscriber]: Frame: {msg.header.frame_id} | Position: X={msg.pose.position.x}, Y={msg.pose.position.y}")
+    def pose_callback(self, msg):
+        frame = msg.header.frame_id
+        x_pos = msg.pose.position.x
+        y_pos = msg.pose.position.y
+
+        self.get_logger().info(f"[INFO] [PoseTrackingSubscriber]: Frame: {frame} | Position: X={x_pos:.2f}, Y={y_pos: .2f}")
 
 def main(args=None):
     rclpy.init(args=args)
